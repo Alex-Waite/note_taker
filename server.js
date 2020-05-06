f448910979695da57c1df4a1afbfb4883098b273
Ben Cole <benedictcole1@gmail.com>
2020-05-06 13:28:17 +0930

    created server page - listening on port 3000

--- server.js
+++ server.js
@@ -8,6 +8,38 @@ const path = require('path');
 // store the port in a variable
 const PORT = 3000;
 
+// console.log(__dirname)
+
+// routes  -------------------------------------------------------
+
+app.get('/notes', function(req , res) {
+    res.sendfile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('*', function(req , res) {
+    res.sendfile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/api/notes', function(req , res) {
+    res.sendfile(path.join(__dirname, '/db/db.json'));
+});
+
+app.post('/api/notes', function(req , res) {
+    res.sendfile(path.join(__dirname, '/db/db.json'));
+});
+
+
+
+
+
+
+
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+
 
 
 

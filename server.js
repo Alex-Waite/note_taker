2482ed492ce23a26ca73be4387299dc514c471cf
Ben Cole <benedictcole1@gmail.com>
2020-05-06 11:58:55 +0930

    set up basic environment for server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+//imports express and stores it an the 'app' variable for easy use
+const express = require('express');
+const app = express();
+
+// get the npm path module to handle routes
+const path = require('path');
+
+// store the port in a variable
+const PORT = 3000;
+
+
+
+
